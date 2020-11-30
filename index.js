4a318e282d965943667610640eececcbbd1e9f4e
Thanh Nguyen <tienthanh8490@gmail.com>
2020-11-30 22:49:12 +0100

    get songs

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ require('dotenv').config()
 
 const axios = require('axios')
 
-const CLIENT_ID = process.env.CLIENT_ID
-const CLIENT_SECRET = process.env.CLIENT_SECRET
 const CLIENT_ACCESS_TOKEN = process.env.CLIENT_ACCESS_TOKEN
+const artist = 'Imagine Dragons'
 
 const request = async (url, params) => {
   try {
@@ -34,9 +33,39 @@ const getArtistId = async (artist) => {
   return searchResults?.response?.hits?.find((hit) => hit?.result?.primary_artist?.name === artist)?.result?.primary_artist?.id
 }
 
+const getSongs = async (artistId) => {
+  let songs = []
+  let page = 1
+
+  while (true) {
+    const result = await request(`artists/${artistId}/songs`, {page})
+
+    console.log({result})
+
+    if (result.meta.status !== 200 || result.response.songs.length === 0) {
+      break
+    }
+
+    songs = [...songs, ...result.response.songs.map((song) => song.title)]
+
+    page += 1
+  }
+
+  return songs
+}
+
 const main = async () => {
-  const artistId = await getArtistId('Imagine Dragons')
-  console.log({artistId})
+  const artistId = await getArtistId(artist)
+  if (!artistId){
+    console.log('No such artist found.')
+    return []
+  }
+
+  const songs = await getSongs(artistId)
+
+  console.log(JSON.stringify(songs))
+
+  return songs
 }
 
 main()
